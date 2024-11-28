98b6b855c917c974e2f6c633b4661f1fddd997d2
Ankit Grai <ankigray@outlook.com>
2024-11-28 11:41:16 +0530

    check new build time [build.apk]

--- lib/Functions/Function.dart
+++ lib/Functions/Function.dart
@@ -69,7 +69,7 @@ Future<void> snackString(
       debugPrint(stackTrace.toString());
     }
   } else {
-    debugPrint('No valid context or string provided.');
+    debugPrint('No valid context or string provided and this is perfect for my testing to be removed or reverted.');
   }
 }
 
